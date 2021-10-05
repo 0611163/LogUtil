f9a378dfd80aa54211c5a959bc280208b3811c3e
suxiang <651029594@qq.com>
2021-10-05 15:33:57 +0800

    初始

---
+++ LogUtil.Test/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace LogUtilTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(654, 540);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 581);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogUtil测试";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LogUtil.Test/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utils;
+
+namespace LogUtilTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ThreadPool.SetMinThreads(20, 20);
+        }
+
+        #region Log
+        private void Log(string log)
+        {
+            if (!this.IsDisposed)
+            {
+                if (this.InvokeRequired)
+                {
+                    this.BeginInvoke(new Action(() =>
+                    {
+                        textBox1.AppendText(DateTime.Now.ToString("HH:mm:ss.fff") + " " + log + "\r\n\r\n");
+                    }));
+                }
+                else
+                {
+                    textBox1.AppendText(DateTime.Now.ToString("HH:mm:ss.fff") + " " + log + "\r\n\r\n");
+                }
+            }
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogUtil.Log("测试写 Info 日志");
+            LogUtil.Debug("测试写 Debug 日志");
+            LogUtil.Error("测试写 Error 日志");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Log("==== 开始 ========");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            List<Task> taskList = new List<Task>();
+
+            Task.Run(() =>
+            {
+                int n = 10000;
+                for (int i = 0; i < n; i++)
+                {
+                    Task task = LogUtil.Log("测试日志 " + i.ToString("000000"));
+                    taskList.Add(task);
+                }
+            });
+
+            Task.Run(() =>
+            {
+                int n = 10000;
+                for (int i = 0; i < n; i++)
+                {
+                    Task task = LogUtil.Debug("测试日志 " + i.ToString("000000"));
+                    taskList.Add(task);
+                }
+            });
+
+            Task.Run(() =>
+            {
+                int n = 10000;
+                for (int i = 0; i < n; i++)
+                {
+                    Task task = LogUtil.Error("测试日志 " + i.ToString("000000"));
+                    taskList.Add(task);
+                }
+            });
+
+            Task.WaitAll(taskList.ToArray());
+
+            Log("==== 结束 " + "，耗时：" + stopwatch.Elapsed.TotalSeconds.ToString("0.000") + " 秒 ========");
+            stopwatch.Stop();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            Log("==== 开始 ========");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            List<Task> taskList = new List<Task>();
+
+            await Task.Run(() =>
+            {
+                int n = 10000;
+                for (int i = 0; i < n; i++)
+                {
+                    Task task = LogUtil.Log("测试日志 " + i.ToString("000000"));
+                    taskList.Add(task);
+                }
+            });
+
+            await Task.Run(() =>
+            {
+                int n = 10000;
+                for (int i = 0; i < n; i++)
+                {
+                    Task task = LogUtil.Debug("测试日志 " + i.ToString("000000"));
+                    taskList.Add(task);
+                }
+            });
+
+            await Task.Run(() =>
+            {
+                int n = 10000;
+                for (int i = 0; i < n; i++)
+                {
+                    Task task = LogUtil.Error("测试日志 " + i.ToString("000000"));
+                    taskList.Add(task);
+                }
+            });
+
+            foreach (Task tsk in taskList)
+            {
+                await tsk;
+            }
+
+            Log("==== 结束 " + "，耗时：" + stopwatch.Elapsed.TotalSeconds.ToString("0.000") + " 秒 ========");
+            stopwatch.Stop();
+        }
+    }
+}
