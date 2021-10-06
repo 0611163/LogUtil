99992a7f9096c04f741f01f2b5da3c48fd435039
suxiang <651029594@qq.com>
2021-10-06 18:01:48 +0800

    测试代码

--- LogUtil.Test/Form1.Designer.cs
+++ LogUtil.Test/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
-            this.button2.Text = "button2";
+            this.button2.Text = "LogUtil";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
